c52c9d39258270ea2fb47c480a96d9fdb7ce21c5
BillKratzIII <wkratz3@gmail.com>
2016-08-11 12:22:47 -0400

    added error handling

--- src/baseball_scoring/Batter.java
+++ src/baseball_scoring/Batter.java
@@ -3,75 +3,84 @@ package baseball_scoring;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-
+/*
+ * Class batter holds individual record of at bats and methods for objects of Batter class
+ */
 public class Batter {
+	//declare variables
 	double battingAverage;
 	double sluggingPercentage;
 	String batterName;
 	int numAtBats;
-	ArrayList<Integer> atBatResults = new ArrayList<>();
+	ArrayList<Integer> atBatResults = new ArrayList<>();//ArrayList to store each at bat result
 	
+	//Batter constructor
 	public Batter(String name){
 		batterName = name;
 	}
-
+	
+	//method to return Batter's batting average
 	public double getBattingAverage() {
 		return battingAverage;
 	}
 
+	//method to return Batter's slugging percentage
 	public double getSluggingPercentage() {
 		return sluggingPercentage;
 	}
 
+	//method to return Batter's name
 	public String getBatterName() {
 		return batterName;
 	}
 
+	//method to prompt user and store number of at bats to get results for
+	public void setNumAtBats() {
+		Scanner sn = new Scanner(System.in);
+		System.out.println("How many at bats would you like to enter for " + this.getBatterName() + ":");
+		
+		try{	//try to take in user input as an integer
+			numAtBats = sn.nextInt();
+		}catch(InputMismatchException d){	//catch error if user enters a non-integer, re-starts method
+			System.out.println("Please enter a valid number greater than 0");
+			setNumAtBats();
+		}
+		if (numAtBats>0){	//ensures user enters a valid number of at bats to track
+			getResults(numAtBats);
+		}else{
+			System.out.println("Not a valid number of at bats");
+			setNumAtBats();
+		}
+	}
+	
+	//method to return number of at bats for the Batter
 	public int getNumAtBats() {
 		return numAtBats;
 	}
-
-	public void setNumAtBats(int numAtBats) {
-		this.numAtBats = numAtBats;
-	}
 	
-	public void getResults(){
+	//method to take in number of at bats for the Batter and collect the result of each individual at bat
+	public void getResults(int atBats){
 		Scanner sc = new Scanner(System.in);
-		Scanner sn = new Scanner(System.in);
-		int atBats=0;
-		System.out.println("How many at bats would you like to enter for " + this.getBatterName() + ":");
 		
-		try{
-			atBats = sn.nextInt();
-		}catch(InputMismatchException d){
-			System.out.println("Please enter a valid number greater than 0");
-			getResults();
-		}
-			if (atBats>0){
-				this.setNumAtBats(atBats);
-				for(int i = 0; i<this.numAtBats; i++){
-					int result = 0;
-					System.out.println("Enter the result of at bat #" + (i+1));
-					try{
-					result = sc.nextInt();
-					}catch(InputMismatchException f){
-						System.out.println("Invalid entry, must be number. Please enter 0,1,2,3 or 4. Start over.");
-						Scoring.main(null);
-					}
-					if(isValid(result)){
-						this.atBatResults.add(result);
-					}else{
-						System.out.println("Invalid entry, please enter 0,1,2,3 or 4");
-						i--;
-					}
+		
+			for(int i = 0; i<atBats; i++){
+				String userInput = null;
+				int result = 0;
+				System.out.println("Enter the result of at bat #" + (i+1));
+				userInput = sc.nextLine();
+					
+				if(isValid(userInput)){	//call method isValid to check that the user entered a valid result for that at bat
+					result = Integer.parseInt(userInput);
+					this.atBatResults.add(result);
+				}else{	//if invalid entry, loop at same iteration until valid entry is received
+					System.out.println("Invalid entry, please enter 0,1,2,3 or 4");
+					i--;
 				}
-			}else{
-				System.out.println("Not a valid number of at bats");
-				getResults();
 			}
 		
 	}
 	
+	//method to calculate batting average based on user inputed at bat results
 	public void calcBattingAverage(){
 		int numHits = 0;
 		for(int i = 0; i<atBatResults.size(); i++){
@@ -82,6 +91,7 @@ public class Batter {
 		this.battingAverage = (((double)numHits)/((double)this.numAtBats));
 	}
 	
+	//method to calculate slugging percentage based on user inputed at bat results
 	public void calcSluggingPercentage(){
 		int totalBases = 0;
 		for (int i=0; i<atBatResults.size(); i++){
@@ -90,14 +100,24 @@ public class Batter {
 		this.sluggingPercentage = (((double)totalBases)/((double)this.getNumAtBats()));
 	}
 	
-	public static boolean isValid(int hitValue){
+	//method to check that an at bat entry is valid. First checks that an integer was entered, then compares the 
+	//entry to a array of valid entries. Returns true of false.
+	public static boolean isValid(String input){
 		boolean isValid = false;
 		int[] validNums = {0,1,2,3,4};
-		for (int i=0; i<validNums.length; i++){	
-			if(hitValue == validNums[i]){
-				isValid = true;
+		int hitValue;
+		
+		try{
+			hitValue = Integer.parseInt(input);
+			for (int i=0; i<validNums.length; i++){	
+				if(hitValue == validNums[i]){
+					isValid = true;
+				}
 			}
+		}catch (NumberFormatException e){
+			
 		}
+		
 		return isValid;
 	}
 }
--- src/baseball_scoring/Scoring.java
+++ src/baseball_scoring/Scoring.java
@@ -5,64 +5,90 @@ import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/*
+ * Scoring class prompts user to enter how many batters they would like to calculate and set's their names. Then they enter how many at bats
+ * to track for each batter. We then populate the results of each at bat. After all at bats are entered for all players, the individual
+ * players batting averages and slugging percentages are displayed. 
+ */
 public class Scoring {
 
 	public static void main(String[] args) {
+		// declare variables
 		String playerName;
-		int numBatters = getNumBatters();
-		ArrayList<Batter> playerList = new ArrayList<>();
-		
-		for(int i=0; i<numBatters; i++){
-			playerName = getBatterNames();
-			playerList.add(new Batter(playerName));
+		int numAtBats;
+		ArrayList<Batter> playerList = new ArrayList<>();// arrayList to hold
+															// batter objects
+															// created by the
+															// user and their
+															// individual stats
+
+		int numBatters = getNumBatters();// calls method to prompt user and
+											// capture number of batters they
+											// would like to enter, returns
+											// number of batters if valid entry
+
+		// loop to prompt user to get names for all the new batters they would
+		// like to enter
+		for (int i = 0; i < numBatters; i++) {
+			playerName = getBatterNames();// method call to return batter's name
+											// as a String
+			playerList.add(new Batter(playerName));//create new player object and store in playerList arrayList
 		}
 		System.out.println();
 		
-		for(int i=0; i<numBatters; i++){ 
+		//loop to get number of at bats, then results for each player
+		for (int i = 0; i < numBatters; i++) {
 			System.out.println("Let's enter the results for " + playerList.get(i).getBatterName() + ":");
-			playerList.get(i).getResults();
+			playerList.get(i).setNumAtBats();
 		}
 		
-		for (int i=0; i<numBatters; i++){
+		//loop to call calculate methods and display statistics
+		for (int i = 0; i < numBatters; i++) {
 			System.out.println();
 			playerList.get(i).calcBattingAverage();
 			DecimalFormat numberFormat = new DecimalFormat("#.000");
-			System.out.println(playerList.get(i).getBatterName() + "'s Batting Average: " + numberFormat.format(playerList.get(i).getBattingAverage()));
+			System.out.println(playerList.get(i).getBatterName() + "'s Batting Average: "
+					+ numberFormat.format(playerList.get(i).getBattingAverage()));
 			playerList.get(i).calcSluggingPercentage();
-			System.out.println(playerList.get(i).getBatterName() + "'s Slugging Percentage: " + numberFormat.format(playerList.get(i).getSluggingPercentage()));
+			System.out.println(playerList.get(i).getBatterName() + "'s Slugging Percentage: "
+					+ numberFormat.format(playerList.get(i).getSluggingPercentage()));
 		}
 	}
 	
-	public static int getNumBatters(){
-		int numBatters=0;
+	//method to prompt user to enter how many batters they would like to enter statistics for. After validation the method returns
+	//the number of batters.
+	public static int getNumBatters() {
+		int numBatters = 0;
 		Scanner sc = new Scanner(System.in);
+
 		System.out.println("How many batters would you like to enter?");
-		try{
+		try {
 			numBatters = sc.nextInt();
-		
-			if (numBatters>0){	
+
+			if (numBatters > 0) {
 				System.out.println("Okay, let's set up " + numBatters + " batters.");
 				System.out.println();
-			}else{
+			} else {
 				System.out.println("Please enter a valid number of batters.");
 				main(null);
 			}
-			
-		}catch(InputMismatchException e){
+
+		} catch (InputMismatchException e) {
 			System.out.println("Please enter a valid integer greater than 0.");
 			main(null);
 		}
-		
+
 		return numBatters;
-		
+
 	}
 	
-	public static String getBatterNames(){
+	//Method to prompt user to enter batter names, returns the user input as String
+	public static String getBatterNames() {
 		String name = null;
 		Scanner sq = new Scanner(System.in);
 		System.out.println("Enter batter name:");
 		name = sq.nextLine();
-			
+
 		return name;
 	}
 
